0db7f74cd31176239665a539fe33239bab1f0236
ayano333 <kknami2wave@gmail.com>
2023-06-30 09:39:38 +0900

    pom

--- boot1/test/hello/src/main/java/com/repository/MemberRepository.java
+++ boot1/test/hello/src/main/java/com/repository/MemberRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.model.Member;
 
 public interface MemberRepository extends JpaRepository<Member,Long>{
+
+    void save(java.lang.reflect.Member member);
 }
